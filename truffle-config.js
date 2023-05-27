6675337ac03e113bf0ad932d8acabe1b9ce83144
ytison <yannick.tison@gmail.com>
2023-05-28 00:30:28 +0200

    init commit

---
+++ truffle-config.js
@@ -0,0 +1,64 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider'); 
+require('dotenv').config();
+
+module.exports = {
+  /**
+   * Networks define how you connect to your ethereum client and let you set the
+   * defaults web3 uses to send transactions. If you don't specify one truffle
+   * will spin up a managed Ganache instance for you on port 9545 when you
+   * run `develop` or `test`. You can ask a truffle command to use a specific
+   * network from the command line, e.g
+   *
+   * $ truffle test --network <network-name>
+   */
+
+  networks: {
+    development: {
+      host: "127.0.0.1",     // Localhost (default: none)
+      port: 8545,            // Standard Ethereum port (default: none)
+      network_id: "*",       // Any network (default: none)
+     },
+    //
+    // An additional network, but with some advanced options…
+    // advanced: {
+    //   port: 8777,             // Custom port
+    //   network_id: 1342,       // Custom network
+    //   gas: 8500000,           // Gas sent with each transaction (default: ~6700000)
+    //   gasPrice: 20000000000,  // 20 gwei (in wei) (default: 100 gwei)
+    //   from: <address>,        // Account to send transactions from (default: accounts[0])
+    //   websocket: true         // Enable EventEmitter interface for web3 (default: false)
+    // },
+    //
+    mumbai: {
+       provider: function(){ 
+        return new HDWalletProvider({
+          mnemonic: {phrase: `${process.env.MNEMONIC}`},
+          providerOrUrl: `${process.env.ALCHEMY_URL}${process.env.ALCHEMY_ID}`
+        }
+        )
+      },
+       network_id: 80001       // Mumbai's id
+    },
+  },
+
+  // Set default mocha options here, use special reporters, etc.
+  mocha: {
+    // timeout: 100000
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.8.20",      // Fetch exact version from solc-bin (default: truffle's version)
+      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
+      settings: {          // See the solidity docs for advice about optimization and evmVersion
+        optimizer: {
+          enabled: false,
+          runs: 200
+        },
+        evmVersion: "byzantium"
+       }
+    }
+  },
+
+};
